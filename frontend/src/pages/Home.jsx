924f34075bf094105f5f549d3ba01a56edee8854
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2024-11-06 17:36:09 +0530

    changes

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello</p>
+     <Hero/>
+    </div>
+  )
+}
+
+export default Home
