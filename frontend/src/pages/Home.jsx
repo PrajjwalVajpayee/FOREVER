a6e5e3f4eb5810b0ec5faf926bfe26280048cf2b
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2024-11-09 23:15:35 +0530

    create main section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
-
+import LatestCollection from '../components/LatestCollection'
 const Home = () => {
   return (
     <div>
-      <p>Hello</p>
+     
      <Hero/>
+     <LatestCollection/>
     </div>
   )
 }
