da2b38c9ea2a073252cf97c05a1fbeb5cfbada7b
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2024-11-27 23:55:21 +0530

    added all the components to the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import Ourpolicy from '../components/Ourpolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
 const Home = () => {
   return (
     <div>
      
      <Hero/>
      <LatestCollection/>
-     {/* <BestSeller/> */}
+     <BestSeller/>
+     <Ourpolicy/>
+     <NewsLetterBox/>
     </div>
   )
 }
