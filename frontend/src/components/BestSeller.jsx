b78aa5701b79b7afccac252ac4fefe4869c57635
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2024-11-27 23:53:27 +0530

    create the best seller section

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import  './Title';
+import Title from'./Title'
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
@@ -20,7 +21,13 @@ const BestSeller = () => {
              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis sunt porro, ex labore consequatur reiciendis vitae iste in delectus soluta laudantium, beatae est hic ab accusamus obcaecati cupiditate sequi commodi.
             </p>
         </div>
-      
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          bestSeller.map((item,index)=>(
+            <ProductItem key={index} id ={item._id} name= {item.image} image= {item.image} price ={item.price}/>
+          ))
+        }
+      </div>
 
     </div>
   )
