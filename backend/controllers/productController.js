aa97ee1b327eb9d2222a3820c122356fea9e5a78
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-01-08 00:17:48 +0530

    some changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
-
+import {v2 as cloudinary} from "cloudinary"
+import productModel from '../models/productModel.js'
 //function for add product 
-const addProduct =()=>{
+const addProduct = async (req,res)=>{
   try {
     const {name,description,price,category,subcategory,sizes,bestseller} = req.body;
 
@@ -9,8 +10,32 @@ const addProduct =()=>{
     const image3=req.files.image3 && req.files.image3[0];
     const image4=req.files.image4 && req.files.image4[0];
       const images = [image1,image2,image3,image4].filter((item)=>item!==undefined)
+
+      let imagesUrl =await Promise.all(
+        images.map(async (item)=>{
+          let result = await cloudinary.uploader.upload(item.path,{resource_type:"image"});
+          return result.secure_url;
+        })
+      )
+       const productData = {
+        name,
+        description,
+        category,
+        price:Number(price),
+        subcategory,
+        bestseller:bestseller === "true"? true:false,
+        sizes:JSON.parse(sizes),
+        image:imagesUrl,
+        date:Date.now()
+       }
+
+       const product = new productModel(productData);
+       await product.save();
+
+       res.json({success:true,message:"product added"});
+
     console.log(name,description,price,category,subcategory,sizes,bestseller);
-    console.log(image1,image2,image3,image4);
+    console.log(images);
     res.json({})    
   } catch (error) {
     console.log(error);
@@ -19,17 +44,17 @@ const addProduct =()=>{
   }
 }
 // function for list product
-const listProduct =()=>{
+const listProduct =async(req,res)=>{
 
 }
 // function for removing product
-const removeProduct =()=>{
+const removeProduct =async(req,res)=>{
 
 }
 
 // function for single product info
-const singleProduct =()=>{
+const singleProduct =async(req,res)=>{
 
 }
 
-export default {addProduct,listProduct,removeProduct,singleProduct}
+export {addProduct,listProduct,removeProduct,singleProduct}
