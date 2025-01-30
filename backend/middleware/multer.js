b4248c69677f8fb21657b187379ca749a75540b6
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-01-30 23:47:31 +0530

    update this section

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
     filename:function(req,file,callback){
         callback(null,file.originalname);
     }
